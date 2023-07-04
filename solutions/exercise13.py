ce5787573b6d7bc53ff0a28cf2904a646a3bec3f
Tobias Stollenwerk <to.stollenwerk@fz-juelich.de>
2023-07-04 10:28:39 +0200

    added VQE exercises

---
+++ solutions/exercise13.py
@@ -0,0 +1,6 @@
+thetas = ParameterVector('θ', num_qubits)
+for i in range(num_qubits):
+    ansatz.h(i)
+    ansatz.ry(thetas[i], i)
+for i in range(num_qubits):
+    ansatz.cx(i, (i + 1) % num_qubits)
